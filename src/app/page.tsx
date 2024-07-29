361c0ef3d61f8a9732ce979a26ac01dfd074ccc6
Leslos <lesliesamafful@gmail.com>
2024-07-29 17:02:58 +0100

    feat: Add Feed, AddPost, LeftMenu, and RightMenu components
    The commit adds new components to the project: Feed, AddPost, LeftMenu, and RightMenu. These components are necessary for the implementation of the homepage layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import LeftMenu from "@/components/LeftMenu"
+import Feed from "@/components/Feed"
+import RightMenu from "@/components/RightMenu"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className='flex gap-6'>
+      <div className="hidden xl:block xl:w-[20%]"><LeftMenu /></div>
+      <div className="w-full lg:w-[70%] xl:w-[50%] "><Feed /></div>
+      <div className="hidden lg:block w-[30%]"><RightMenu /></div>
+
+    </div>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,32 +2,111 @@ import Link from "next/link";
 import React from "react";
 import MobileMenu from "./MobileMenu";
 import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
       {/* LEFT */}
       <div className="md:hidden lg:block width-[20%]">
-        <Link className="font-bold text-xl text-blue-600" href="/">IAMSOCIAL</Link>
+        <Link className="font-bold text-xl text-blue-600" href="/">
+          IAMSOCIAL
+        </Link>
       </div>
       {/* CENTER */}
-      <div className="hidden md:flex w-[50%] text-sm">
+      <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
         <div className="flex gap-6 text-gray-600">
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/home.png" alt="Homepage" width={16} height={16} className="w-4 h-4" />
-          <span>Home</span>
-        </Link>
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/friends.png" alt="Friends" width={16} height={16} className="w-4 h-4" />
-          <span>Friends</span>
-        </Link>
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/stories.png" alt="Stories" width={16} height={16} className="w-4 h-4" />
-          <span>Stories</span>
-        </Link></div>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/home.png"
+              alt="Homepage"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            <span>Home</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/friends.png"
+              alt="Friends"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/stories.png"
+              alt="Stories"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            <span>Stories</span>
+          </Link>
+        </div>
+
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent outline-none"
+          />
+          <Image src={"/search.png"} alt="Search" width={14} height={14} />
+        </div>
       </div>
       {/* RIGHT */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <ClerkLoading>
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-blue-500"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image src="/people.png" alt="People" width={24} height={24} />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/messages.png"
+                alt="Messages"
+                width={20}
+                height={20}
+              />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/notifications.png"
+                alt="Notifications"
+                width={20}
+                height={20}
+              />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <div className="flex items-center gap-2 text-sm">
+            <SignedOut>
+              <Image src="/login.png" alt="Login" width={20} height={20} />
+              <Link href="/sign-in" className="text-black">
+                Login/Register
+              </Link>
+            </SignedOut>
+          </div>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
